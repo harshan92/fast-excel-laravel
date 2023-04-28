fda900ebac8e910e28d75f4529e3fac5fd86bd7d
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-04-28 23:53:21 +0530

    Implented header style

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\TblGl;
 use Rap2hpoutre\FastExcel\FastExcel;
 use Illuminate\Http\Request;
-
+use OpenSpout\Common\Entity\Style\Style;
 class ExcelController extends Controller
 {
     public function to_excel()
     {
+
+        $header_style = (new Style())->setFontBold();
+
+        $rows_style = (new Style())
+            ->setFontSize(15)
+            ->setShouldWrapText()
+            ->setBackgroundColor("EDEDED");
         $gls=TblGl::limit(10000)->get();
-        return (new FastExcel($gls))->download('file.xlsx',function ($gl) {
+        return (new FastExcel($gls))->headerStyle($header_style)->download('file.xlsx',function ($gl) {
             $dr="";
             if($gl->Dr!=0){
                 $dr=number_format($gl->Dr,2,'.',',');
